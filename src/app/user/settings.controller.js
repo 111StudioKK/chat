fa5958b888d6ef4f1da54b70f1e169084dfa8d14
Hugues ALEXANDRE <hugues@111studio.jp>
2015-05-22 12:43:10 +0900

    add settings with the possibility to change user name CHAT-2 #start-review

---
+++ src/app/user/settings.controller.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var settingsCtrl = function(settingsService, userService) {
+    var settingsCtrl = this;
+
+    settingsCtrl.data = {};
+
+    settingsCtrl.save = function() {
+        settingsService.save(settingsCtrl.data);
+        settingsService.close();
+    };
+    settingsCtrl.cancel = function() {
+        settingsService.close();
+    };
+};
+
+angular.module('chat')
+    .service('settingsService', function(userService){
+        var service = this;
+        service.show = function(ctrl) {
+            service.controller.ctrl.data = userService.getConverted(userService.getMe());
+            service.popupElement.removeClass('hidden');
+        };
+
+        service.save = function(userSettings) {
+            userService.modify(userSettings);
+        };
+
+        service.close = function() {
+            service.popupElement.addClass('hidden');
+        };
+    })
+    .controller('settingsCtrl', settingsCtrl)
+    .directive('popupSettings', function(settingsService){
+        // Runs during compile
+        return {
+            controller: 'settingsCtrl as ctrl',
+            templateUrl: 'app/user/settings.html',
+            link: function(ctrl, element) {
+                settingsService.popupElement = element;
+                settingsService.controller = ctrl;
+            }
+        };
+    });
